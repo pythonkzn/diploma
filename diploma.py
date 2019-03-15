a525bc02559466accd369705616e57a65cd4a6c5
raravilov <raravilov@gmail.com>
2019-03-15 16:06:46 +0300

    исправлены замечания: 1, 2, 6

--- diploma.py
+++ diploma.py
@@ -2,6 +2,8 @@ import requests
 import itertools
 import json
 import codecs
+import time
+
 
 TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 
@@ -19,10 +21,10 @@ class User:
     def get_subs_gid(self):
         params = self.get_params()
         response = requests.get(
-            'https://api.vk.com/method/users.getSubscriptions',
+            'https://api.vk.com/method/groups.get',
             params
         )
-        return response.json()['response']['groups']['items']
+        return response.json()['response']['items']
 
     def get_subs_gid_fr(self):
         friends_group_list = []
@@ -39,26 +41,20 @@ class User:
             'v': 5.92
             }
             try: #обходим случаи по которым запрос не отвечает ожидаемым форматом
-                response_get_groups = requests.get('https://api.vk.com/method/users.getSubscriptions', params)
-                friends_group_list.append(response_get_groups.json()['response']['groups']['items']) #получаем список списков групп в которых состоят все друзья user
+                response_get_groups = requests.get('https://api.vk.com/method/groups.get', params)
+                friends_group_list.append(response_get_groups.json()['response']['items']) #получаем список списков групп в которых состоят все друзья user
                 print('-')
+                time.sleep(2)
             except:
-                pass
-        output_list = list(itertools.chain.from_iterable(friends_group_list))
+                print(response_get_groups.json()) #выводим полученные ошибки от API VK
+        output_list = list(itertools.chain.from_iterable(friends_group_list)) #делаем список групп из списка списков
         return output_list
 
-    def uncommomon_group_list(self):
-        user_group_list = self.get_subs_gid()
-        friends_group_list = self. get_subs_gid_fr()
-        common_group = list(set(user_group_list) & set(friends_group_list)) #получаем список общих групп User и его друзей
-        return common_group
-        for group in user_group_list:
-            for group_com in common_group:
-                if group != group_com: #условие на нахождение групп в которых состоит User и не состоят другие пользователи
-                    output_list.append(group)
-                else:
-                    pass
-        return set(output_list)
+    def uncommon_group_list(self):
+        output_list = []
+        user_group_set = set(self.get_subs_gid())
+        friends_group_set = set(self.get_subs_gid_fr())
+        return set(user_group_set.difference(friends_group_set))
 
 
 
@@ -86,16 +82,36 @@ class Group:
         return output_dict
 
 
-user = User(171691064)
-uncommon_groups = user.uncommomon_group_list()
+
+user_input = input('Введите id или имя пользователя: ')
+
+try:
+    int(user_input)
+    user = User(user_input)
+except ValueError:
+    params = {
+        'user_ids': user_input,
+        'access_token': TOKEN,
+        'v': 5.92
+    }
+    response_get_id = requests.get(
+        'https://api.vk.com/method/users.get',
+        params
+    )
+    user = User(int(response_get_id.json()['response'][0]['id'])) #получили по имени пользователя его id
+
+
 output_list = []
+uncommon_groups = user.uncommon_group_list()
+
 for group in uncommon_groups:
     try:
         group = Group(group)
         output_list.append(group.get_group_data())
     except:
         pass
-print(output_list)
+
 with codecs.open('groups.json', 'w', encoding='utf-8') as json_file:
     json.dump(output_list, json_file, ensure_ascii=False)
+print(uncommon_groups)
 
